0ae1ee2292bde2edf0081a1a3538bea92a8ae013
Joel <joelbaraka4325@gmail.com>
2024-01-24 18:47:52 +0300

    implementing shadcdn

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 type Props = {
   params: {
     id: number;
@@ -9,8 +11,9 @@ type Props = {
 
 function genre({ params: { id }, searchParams: { genre } }: Props) {
   return (
-    <div>
+    <div className="text-red-500">
       welcome to the genre with ID: {id} and name: {genre}
+      <Button variant={"ghost"}>Click Me</Button>
     </div>
   );
 }
