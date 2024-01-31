fe9a5e5be5193f266bdade52a0ae1af682a16068
Joel <joelbaraka4325@gmail.com>
2024-01-31 23:56:22 +0300

    adding flex to the parent container

--- components/CarouselBanner.tsx
+++ components/CarouselBanner.tsx
@@ -17,7 +17,7 @@ function CarouselBanner({ movies }: { movies: Movie[] }) {
       className="overflow-hidden lg:-mt-40 relative cursor-pointer"
       ref={emblaRef}
     >
-      <div>
+      <div className="flex">
         {movies.map((movie) => (
           <div key={movie.id} className="flex-full min-w-0 relative">
             <Image
@@ -28,12 +28,12 @@ function CarouselBanner({ movies }: { movies: Movie[] }) {
               height={1090}
             />
 
-            {/* <div className="hidden lg:inline absolute mt-0 top-0 pt-40 xl:pt-52 left-0 lg:mt-40 bg-transparent z-20 h-full w-full bg-gradient-to-r from-gray-900/90 via-transparent to-transparent p-10 space-y-5 text-white ">
+            <div className="hidden lg:inline absolute mt-0 top-0 pt-40 xl:pt-52 left-0 lg:mt-40 bg-transparent z-20 h-full w-full bg-gradient-to-r from-gray-900/90 via-transparent to-transparent p-10 space-y-5 text-white ">
               <h2 className="text-5xl font-bold max-w-xl z-50">
                 {movie.title}
               </h2>
               <p className="max-w-xl line-clamp-3">{movie.overview}</p>
-            </div> */}
+            </div>
           </div>
         ))}
       </div>
