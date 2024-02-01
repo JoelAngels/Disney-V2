ecb0acae8a0231dad64eb883ad37a11262aaad90
Joel <joelbaraka4325@gmail.com>
2024-02-01 18:22:09 +0300

    search page done

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,5 +1,5 @@
-"use client";
-
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -8,17 +8,27 @@ type Props = {
   };
 };
 
-function SearchPage({ params: { term } }: Props) {
+async function SearchPage({ params: { term } }: Props) {
   if (!term) notFound();
 
   const termToUse = decodeURI(term);
 
   //   Api call to get the movies
+  const movies = await getSearchedMovies(termToUse);
+  const popularMovies = await getPopularMovies();
   //Api call to get the Popular Movies
 
   return (
-    <div>
-      <h1>Welcome to the search PAGE: {termToUse}</h1>
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {termToUse}</h1>
+        <MoviesCarousel title="Movies" movies={movies} isVertical />
+        <MoviesCarousel
+          title="You may also like"
+          movies={popularMovies}
+          isVertical
+        />
+      </div>
     </div>
   );
 }
